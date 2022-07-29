ceb301dcf8466a49e4043321ff790020fbf8155c
Matthew James <mjames4@woolworths.com.au>
2022-07-29 12:27:50 +1000

    Define order expectations

---
+++ test/order_test.dart
@@ -0,0 +1,73 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("Given an order is pending", () {
+    test("Then the order's state is 'pending'", () {
+      throw UnimplementedError();
+    });
+
+    group("When the order is marked en route", () {
+      test("Then the order's state is 'en route'", () {
+        throw UnimplementedError();
+      });
+    });
+
+    group("When the order is marked arrived", () {
+      test("Then an OrderStateException is thrown", () {
+        throw UnimplementedError();
+      });
+    });
+
+    group("When the ETA is updated", () {
+      test("Then the order's ETA is successfully updated", () {
+        throw UnimplementedError();
+      });
+    });
+  });
+
+  group("Given an order is en route", () {
+    group("When the order is marked pending", () {
+      test("Then an OrderStateException is thrown", () {
+        throw UnimplementedError();
+      });
+    });
+
+    group("When the order is marked arrived", () {
+      test("Then the order's state is 'arrived'", () {
+        throw UnimplementedError();
+      });
+    });
+
+    group("When the ETA is updated", () {
+      test("Then the order's ETA is successfully updated", () {
+        throw UnimplementedError();
+      });
+    });
+  });
+
+  group("Given an order is arrived", () {
+    group("When the order is marked en route", () {
+      test("Then an OrderStateException is thrown", () {
+        throw UnimplementedError();
+      });
+    });
+
+    group("When the ETA is updated", () {
+      test("Then an EtaUpdateException is thrown", () {
+        throw UnimplementedError();
+      });
+    });
+
+    group("When the order is marked complete", () {
+      test("Then the order's state is 'complete'", () {
+        throw UnimplementedError();
+      });
+    });
+  });
+
+  group("Given an order is complete", () {
+    test("Then the customer signature is available", () {
+      throw UnimplementedError();
+    });
+  });
+}
