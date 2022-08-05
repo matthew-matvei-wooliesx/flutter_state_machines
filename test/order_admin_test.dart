8f48fa56b3558658fbd973bb918ccbada41fae1e
Matthew James <mjames4@woolworths.com.au>
2022-08-05 11:39:22 +1000

    Implement presenting 'New' button

--- test/order_admin_test.dart
+++ test/order_admin_test.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_machines/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("Given an order has not yet been created", () {
-    testWidgets("Then a button to create a new route is shown", (tester) {
-      throw UnimplementedError();
+    testWidgets("Then a button to create a new route is shown", (tester) async {
+      await tester.pumpWidget(const OrderAdmin());
+      expect(find.widgetWithText(ElevatedButton, "New"), findsOneWidget);
     });
   });
 
