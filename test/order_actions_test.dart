ee65a9793315bd460133b0c64f6a7a1b756f3b6b
Matthew James <mjames4@woolworths.com.au>
2022-08-05 18:16:48 +1000

    Ensure new action remains when order exists

--- test/order_actions_test.dart
+++ test/order_actions_test.dart
@@ -1,3 +1,4 @@
+import 'package:clock/clock.dart';
 import 'package:flutter_state_machines/order.dart';
 import 'package:flutter_state_machines/order_actions.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -28,6 +29,39 @@ void main() {
         expect(orderActions, hasLength(1));
 
         orderActions.first();
+
+        verify(mockNewOrderCallback()).called(1);
+      });
+    });
+  });
+
+  group("Given a new order", () {
+    final Order order = Order(eta: clock.now());
+
+    group("When getting Order Actions", () {
+      late _ActionCallback mockNewOrderCallback;
+      late List<OrderAction> orderActions;
+
+      setUp(() {
+        mockNewOrderCallback = _MockActionCallback();
+        orderActions = OrderActions.from(
+          order,
+          callbacks: OrderActionsCallbacks(newOrder: mockNewOrderCallback),
+        );
+      });
+
+      test("Then a button for creating a new order is returned", () {
+        expect(orderActions, isNotEmpty);
+        expect(
+          orderActions,
+          contains(predicate((OrderAction action) => action.label == "New")),
+        );
+      });
+
+      test("Then an action can be invoked to create a new order", () {
+        expect(orderActions, isNotEmpty);
+
+        orderActions.firstWhere((action) => action.label == "New").call();
 
         verify(mockNewOrderCallback()).called(1);
       });
