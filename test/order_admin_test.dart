de4c18d2f89466c24687935da0f6d61a2d641509
Matthew James <mjames4@woolworths.com.au>
2022-08-05 11:22:10 +1000

    Define initial expectations for admin page

---
+++ test/order_admin_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("Given an order has not yet been created", () {
+    testWidgets("Then a button to create a new route is shown", (tester) {
+      throw UnimplementedError();
+    });
+  });
+
+  group("Given an order exists", () {
+    testWidgets("Then the order's ID is shown", (tester) {
+      throw UnimplementedError();
+    });
+
+    testWidgets("Then a button to create a new route is shown", (tester) {
+      throw UnimplementedError();
+    });
+  });
+}
